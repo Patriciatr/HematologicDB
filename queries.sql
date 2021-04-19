71081d3c681b8c6330eacda1ab9ebbf8d92ab50b
Patriciatr <patriciatr99@uma.es>
2021-04-19 11:14:46 +0200

    Update Query Design

--- queries.sql
+++ queries.sql
@@ -11,26 +11,32 @@ SELECT Name,Proteome
 From hematologicDB.Protein 
 WHERE Proteome IN ("Chromosome 3", "Chromosome 7","Chromosome X");
 
-#Select the Name and proteome of the proteins with the symbol and type of the genes that correspond to them
-SELECT p.Name, p.Proteome, g.Symbol, g.Type FROM hematologicDB.Protein p INNER JOIN hematologicDB.Genes g 
+#Select the Name and proteome of the proteins with the symbol and type of the genes "KMT2A", "BCL10","BCL6", "HFE"
+SELECT p.Name, p.Proteome, g.Symbol, g.Type 
+FROM hematologicDB.Protein p INNER JOIN hematologicDB.Genes g 
 ON p.Genes_idGenes = g.idGenes
+WHERE Symbol IN ("KMT2A", "BCL10","BCL6", "HFE")
 order by p.Proteome;
 
-#Select the Name and ICD10 with their respective name and sequence of the protein 
-SELECT d.Name, d.ICD_10, p.Name, p.Sequence  FROM hematologicDB.Disease d LEFT JOIN hematologicDB.Protein p 
+#Select the Name and ICD10 with their respective name and sequence of the protein relate to histone
+SELECT d.Name, d.ICD_10, p.Name, p.Sequence  
+FROM hematologicDB.Disease d LEFT JOIN hematologicDB.Protein p 
 ON d.idDisease = p.Disease_idDisease 
+WHERE Molec_function LIKE '%histone%'
 group by d.Name, d.ICD_10, p.Name, p.Sequence 
 order by d.ICD_10;
 
 # SELECT PROTEIN NAMES WITH MOLECULAR AND BIOLOGICAL FUNCTIONS FOR GENES "KMT2C","KMT2A","BCL6"
 SELECT Name , Molec_function,Bio_proces
 FROM hematologicDB.Protein 
-WHERE Genes_idGenes IN (SELECT idGenes FROM hematologicDB.Genes WHERE Symbol IN ("KMT2C","KMT2A","BCL6"));
+WHERE Genes_idGenes IN (SELECT idGenes FROM hematologicDB.Genes 
+WHERE Symbol IN ("KMT2C","KMT2A","BCL6"));
 
 #SELECT THE PROTEINS WHICH DISEASE HAS THE CODE ICD10 C91 OR C92
 SELECT Name , Proteome 
 FROM hematologicDB.Protein
-WHERE Disease_idDisease IN (SELECT idDisease FROM hematologicDB.Disease WHERE ICD_10=" C91" OR ICD_10="C92")
+WHERE Disease_idDisease IN (SELECT idDisease FROM hematologicDB.Disease 
+WHERE ICD_10=" C91" OR ICD_10="C92")
 
 
 
